b7c3661860bb97f6b936b83ec565f307d118eb9e
Tortitas <victorgf2011@gmail.com>
2023-04-12 22:17:27 +0200

    homepage install

--- browser/install.sh
+++ browser/install.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
 
 # Firefox
+profiles=$(ls -d ~/.mozilla/firefox/*/)
+echo "Firefox profiles:"
+echo $profiles | tr ' ' '\n'
+echo
+
+echo "Enter the profile you want to install the homepage to:"
+read profile
+
+if [[ ! $profiles =~ $profile ]]; then
+    echo "Invalid profile"
+    exit 1
+fi
+
+echo "Installing homepage to $profile"
+mkdir -p "$profile/homepage"
+cp -r homepage/* "$profile/homepage"
+
+echo "Please close Firefox"
+read -p "Press enter to continue"
+
+cp "$profile/prefs.js" "$profile/prefs.js.bck"
+
+is_homepage_set=$(grep "browser.startup.homepage\", " "$profile/prefs.js")
+if [[ $is_homepage_set ]]; then
+    echo "Homepage already set"
+    exit 1
+fi
+
+echo "user_pref(\"browser.startup.homepage\", \"file://$profile/homepage/index.html\");" >> "$profile/prefs.js"
+
+echo "Profile installed!"
 
