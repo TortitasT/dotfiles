a38ee3bf126abc6ffaa941f7d1d99aeaa27a842e
Tortitas <victorgf2011@gmail.com>
2023-04-12 16:11:47 +0200

    newsboat skin

--- newsboat/install.sh
+++ newsboat/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Copy config
+cp config ~/.newsboat/config
 cp urls ~/.newsboat/urls
+
+# Install theme
+curl -o ~/.newsboat/dark https://raw.githubusercontent.com/catppuccin/newsboat/main/themes/dark
