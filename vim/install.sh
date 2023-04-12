89f55c2c5cd0ad95228914ad1907d6b695e6c852
Tortitas <victorgf2011@gmail.com>
2023-04-12 10:16:51 +0200

    neovim 0.9 :D

--- vim/install.sh
+++ vim/install.sh
@@ -6,6 +6,16 @@ cp ./init.vim ~/.config/nvim/init.vim
 cp ./coc-settings.json ~/.config/nvim/coc-settings.json
 cp -rf ./ftplugin ~/.config/nvim/
 
+# Continue?
+echo "Continue? (y/n)"
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    echo "Installing..."
+else
+    echo "Aborting..."
+    exit 1
+fi
+
 nvim +PlugInstall +qall
 
 cocExtensions="coc-tsserver coc-eslint coc-json coc-prettier coc-css coc-java coc-vetur @yaegassy/coc-intelephense coc-sh coc-blade @yaegassy/coc-phpstan coc-pyright"
