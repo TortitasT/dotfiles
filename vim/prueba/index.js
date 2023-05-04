b2d03051389c5ee28de8d04b4f0c544ee4cddbe1
TortitasT <victorgf2011@gmail.com>
2023-05-04 23:31:54 +0200

    i hate windows

--- vim/prueba/index.js
+++ vim/prueba/index.js
@@ -1,29 +1,33 @@
-const a = console.log("aa");
-console.log("Holaa")
-console.log("hoasd")
+const a = console.log('aa')
+console.log('Holaa')
+console.log('hoasd')
 
-console.log("hoasd")
+console.log('hoasd')
 
-console.log("hoaaa");
+console.log('hoaaa')
 
-console.log(
-  () => {
-    console.log("hpoña")
+console.log(() => {
+  console.log('hpoña')
 
-    console.log("hpoña")
-    console.log("hpoña")
+  console.log('hpoña');
+  console.log('hpoña')
+
+  console.log('hpoña')
+
+  console.log('hopo')
+
+  makeid = length => {
 
 
-    console.log("hpoña")
 
-    makeid = (length) => {
-      var result = '';
-      var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      var charactersLength = characters.length;
-      for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      }
-      return result;
+
+    var result = ''
+    var characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var charactersLength = characters.length
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength))
     }
+    return result
   }
-)
+})
