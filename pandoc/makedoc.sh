b629c95986aa15864f8fcff3a88a9e5edfb35a92
Tortitas <victorgf2011@gmail.com>
2023-05-07 23:23:21 +0200

    makedoc for unix

---
+++ pandoc/makedoc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+css_url="https://raw.githubusercontent.com/tortitast/dotfiles/master/pandoc/pandoc.css"
+
+wget -o pandoc.tmp.css $css_url
+
+for filename in *.md
+do
+  filenameOutput="${filename%.*}.html"
+
+  pandoc $filename -o $filenameOutput -t html --css pandoc.tmp.css
+done
+
+rm pandoc.tmp.css
