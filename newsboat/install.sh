1d45792c714d755e1295b6b7f9412c0e469f9a3f
Tortitas <victorgf2011@gmail.com>
2023-06-23 13:57:30 +0200

    uinstall newsboar

--- newsboat/install.sh
+++ newsboat/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Copy config
-cp config ~/.newsboat/config
-cp urls ~/.newsboat/urls
+rm ~/.newsboat/config
+ln -s $PWD/config ~/.newsboat/config
+
+rm ~/.newsboat/urls
+ln -s $PWD/urls ~/.newsboat/urls
 
 # Install theme
 curl -o ~/.newsboat/dark https://raw.githubusercontent.com/catppuccin/newsboat/main/themes/dark
