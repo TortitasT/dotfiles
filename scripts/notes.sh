050b9b5a934f1269a2954c20a6a46e29d6aebb8d
Tortitas <victorgf2011@gmail.com>
2023-07-14 11:58:15 +0200

    chore: add .swp detection to notes

--- scripts/notes.sh
+++ scripts/notes.sh
@@ -7,6 +7,11 @@ if [ ! -d $BASE_PATH ]; then
 fi
 
 if [ $# -eq 0 ]; then
+    if [ -f $BASE_PATH/.notes.md.swp ]; then
+      echo "There is a swap file, you are probably editing this on another terminal already."
+      exit 1
+    fi
+
     vim $BASE_PATH/notes.md
     exit 1
 fi
@@ -18,6 +23,11 @@ fi
 
 if [ $1 = "rm" ]; then
     rm $BASE_PATH/$2
+    exit 1
+fi
+
+if [ -f "$BASE_PATH/.$1.swp" ]; then
+    echo "There is a swap file, you are probably editing this on another terminal already."
     exit 1
 fi
 
