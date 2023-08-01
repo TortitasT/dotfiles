2b88a4e36cf40d5d761931a456705cea0b4133cd
Victor Garcia Fernandez <victorgf2011@gmail.com>
2023-08-01 10:37:57 +0200

    feat: fix dap if not installed

--- vim/lua/tortitas/dap.lua
+++ vim/lua/tortitas/dap.lua
@@ -1,9 +1,35 @@
 local dap = require 'dap'
 
-dap.adapters.php = {
-  type = "executable",
-  command = "node",
-  args = { os.getenv("HOME") .. "/build/vscode-php-debug/out/phpDebug.js" }
-}
+loadPhpDebug()
 
-require('dap.ext.vscode').load_launchjs(os.getenv('HOME') .. '/.vscode/launch.json')
+function loadPhpDebug()
+  if not file_exists(get_home_path() .. '/build/vscode-php-debug/out/phpDebug.js') then
+    return
+  end
+
+  dap.adapters.php = {
+    type = 'executable',
+    command = 'node',
+    args = { os.getenv('HOME') .. '/build/vscode-php-debug/out/phpDebug.js' },
+  }
+
+  require('dap.ext.vscode').load_launchjs(os.getenv('HOME') .. '/.vscode/launch.json')
+end
+
+function file_exists(name)
+  local f = io.open(name, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
+function get_home_path()
+  local path = os.getenv('HOME')
+  if path == nil then
+    path = os.getenv('USERPROFILE') -- Windoooowss
+  end
+  return path
+end
