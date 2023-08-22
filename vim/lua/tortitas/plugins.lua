3032f7b793fe5a4cb2b0c8785087d926d73fef93
Tortitas <victorgf2011@gmail.com>
2023-08-22 15:06:00 +0200

    (vim) chore: comment local langkeeper

--- vim/lua/tortitas/plugins.lua
+++ vim/lua/tortitas/plugins.lua
@@ -11,8 +11,8 @@ return {
 
   {
     'tortitast/langkeeper.nvim',
-    dir = '/home/tortitas/Git/langkeeper.nvim',
-    dev = true,
+    -- dir = '/home/tortitas/Git/langkeeper.nvim',
+    -- dev = true,
     config = function()
       require 'langkeeper'.setup()
     end
