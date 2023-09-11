dba8a036fc4f208c5fc857728dca650d2cb4465b
Victor Garcia <victorgf2011@gmail.com>
2023-09-11 21:52:17 +0200

    chore: arch laptop

--- i3/install.sh
+++ i3/install.sh
@@ -2,5 +2,7 @@
 
 source ../lib/installer.sh
 
+ensureDirectories "$HOME/.config" "$HOME/.config/i3"
+
 linkFile "config" "$HOME/.config/i3/config"
 linkFile "wallpaper" "$HOME/.config/i3/wallpaper"
