53deed5307b039faa62234db6ae4f50e609013ad
Victor Garcia <victorgf2011@gmail.com>
2023-09-20 16:18:57 +0200

    (arch) install log

--- eww/install.sh
+++ eww/install.sh
@@ -6,3 +6,14 @@ ensureDirectories "$HOME/.config" "$HOME/.config/eww"
 
 linkFile "eww.yuck" "$HOME/.config/eww/eww.yuck"
 linkFile "eww.scss" "$HOME/.config/eww/eww.scss"
+
+installEww() {
+  curl -sS https://github.com/elkowar.gpg | gpg --import -i -
+  curl -sS https://github.com/web-flow.gpg | gpg --import -i -
+
+  install eww
+}
+
+if ! command -v eww &> /dev/null; then
+  installEww
+fi
