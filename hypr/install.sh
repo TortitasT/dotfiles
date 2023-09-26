188e18537e734129b6bbd752b4a77912fcceab4e
Victor Garcia <victorgf2011@gmail.com>
2023-09-26 20:26:57 +0200

    feat: kitty theme, change wallpaper

--- hypr/install.sh
+++ hypr/install.sh
@@ -6,4 +6,4 @@ ensureDirectories "$HOME/.config" "$HOME/.config/hypr"
 
 linkFile "hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
 linkFile "hyprpaper.conf" "$HOME/.config/hypr/hyprpaper.conf"
-linkFile "../wallpaper/dark-cat-rosewater.png" "$HOME/.config/hypr/wallpaper"
+linkFile "../wallpaper/tech.jpg" "$HOME/.config/hypr/wallpaper"
---
+++ kitty/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ../lib/installer.sh
+
+linkFile kitty.conf "$HOME/.config/kitty/kitty.conf"
+linkFile current-theme.conf "$HOME/.config/kitty/current-theme.conf"
