922459528fdc1da154841788f5ef2862584a1677
Victor Garcia <victorgf2011@gmail.com>
2023-09-27 16:07:48 +0200

    fix(kitty): ensure directories

--- kitty/install.sh
+++ kitty/install.sh
@@ -2,5 +2,7 @@
 
 source ../lib/installer.sh
 
+ensureDirectory "$HOME/.config/kitty"
+
 linkFile kitty.conf "$HOME/.config/kitty/kitty.conf"
 linkFile current-theme.conf "$HOME/.config/kitty/current-theme.conf"
