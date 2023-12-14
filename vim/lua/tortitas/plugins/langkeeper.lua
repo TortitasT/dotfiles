bdc1fa4f758d4bff7e4ca9ac312b07e6fff600e1
Tortitas <victorgf2011@gmail.com>
2023-12-14 10:58:19 +0100

    chore(vim, zsh): disable langkeeper, zsh pnpm

--- vim/lua/tortitas/plugins/langkeeper.lua
+++ vim/lua/tortitas/plugins/langkeeper.lua
@@ -1,7 +1,7 @@
 -- langkeeper will keep track of the languages you use and the time you spend on them.
 return {
   'tortitast/langkeeper.nvim',
-  enabled = true,
+  enabled = false,
   -- dir = '/home/tortitas/Git/langkeeper.nvim',
   -- dev = true,
   config = function()
