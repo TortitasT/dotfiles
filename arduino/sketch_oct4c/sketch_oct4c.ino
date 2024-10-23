00d7746844072c4b1e70b3df3ff2a0832feffde5
Victor Garcia <victorgf2011@gmail.com>
2024-10-23 21:12:16 +0200

    feat(arduino): shutdown pin

--- arduino/sketch_oct4c/sketch_oct4c.ino
+++ arduino/sketch_oct4c/sketch_oct4c.ino
@@ -1,4 +1,6 @@
 #include <DigiKeyboard.h>
+#define PIN_LED 1
+#define PIN_TOUCH 2
 
 int randomBetween(int min, int max) {
   return rand() % (max - min + 1) + min;
@@ -29,9 +31,30 @@ void deleteForward() {
   DigiKeyboard.sendKeyStroke(76);
 }
 
-void setup() {}
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_TOUCH, INPUT_PULLUP);
+
+  // Apagar led verde
+  pinMode(0, OUTPUT);
+  digitalWrite(0, LOW);
+}
+
+int isEnabled = 0;
 
 void loop() {
+  if (digitalRead(PIN_TOUCH) == LOW){
+    isEnabled = 1;
+  } else {
+    isEnabled = 0;
+  }
+
+  if (!isEnabled) {
+    digitalWrite(PIN_LED, LOW);
+    return;
+  }
+
+  digitalWrite(PIN_LED, HIGH);
   pauseLong();
 
   // this is generally not necessary but with some older systems it seems to
