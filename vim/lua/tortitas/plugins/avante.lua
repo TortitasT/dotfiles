90933abe55578929b486d481f8c1dc50399f7038
Victor Garcia <vgarciafer@minsait.com>
2024-11-04 14:07:32 +0100

    chore: disable avante

--- vim/lua/tortitas/plugins/avante.lua
+++ vim/lua/tortitas/plugins/avante.lua
@@ -1,6 +1,7 @@
 return
 {
   "yetone/avante.nvim",
+  enabled = false,
   event = "VeryLazy",
   opts = {
     -- add any opts here
@@ -11,13 +12,5 @@ return
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    {
-      -- Make sure to setup it properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
   },
 }
---
+++ vim/lua/tortitas/plugins/render_markdown.lua
@@ -0,0 +1,8 @@
+return {
+  -- Make sure to setup it properly if you have lazy=true
+  'MeanderingProgrammer/render-markdown.nvim',
+  opts = {
+    file_types = { "markdown", "Avante" },
+  },
+  ft = { "markdown", "Avante" },
+}
